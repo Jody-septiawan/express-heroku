2d7d52dd2bb0842f2557cb752626a6ca13a03ec4
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-02-01 15:17:51 +0700

    update

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello Wolrd',
   });
+});
+
+app.get('/user', (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: 'jody1',
+      email: 'jody1@mail.com',
+      password: '123',
+    },
+    {
+      id: 2,
+      name: 'jody2',
+      email: 'jody2@mail.com',
+      password: '123',
+    },
+    {
+      id: 3,
+      name: 'jody3',
+      email: 'jody3@mail.com',
+      password: '123',
+    },
+  ];
+  res.render('user', { users });
 });
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
