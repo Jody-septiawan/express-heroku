e40a60d232f4ec94c7f361e047c800a83f614bd2
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-02-02 18:32:11 +0700

    v1

--- index.js
+++ index.js
@@ -5,32 +5,37 @@ const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'hbs');
 
-app.get('/hello', (req, res) => {
+const users = [
+  {
+    id: 1,
+    name: 'jody1',
+    email: 'jody1@mail.com',
+    password: '123',
+  },
+  {
+    id: 2,
+    name: 'jody2',
+    email: 'jody2@mail.com',
+    password: '123',
+  },
+  {
+    id: 3,
+    name: 'jody3',
+    email: 'jody3@mail.com',
+    password: '123',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.render('user', { users });
+});
+
+app.get('/home', (req, res) => {
   res.render('index');
 });
 
-app.get('/', (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: 'jody1',
-      email: 'jody1@mail.com',
-      password: '123',
-    },
-    {
-      id: 2,
-      name: 'jody2',
-      email: 'jody2@mail.com',
-      password: '123',
-    },
-    {
-      id: 3,
-      name: 'jody3',
-      email: 'jody3@mail.com',
-      password: '123',
-    },
-  ];
-  res.render('user', { users });
+app.get('/api', (req, res) => {
+  res.send(users);
 });
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
